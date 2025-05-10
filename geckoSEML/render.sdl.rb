31858def250831961dc19ba70d9b49b9f2c9a3d8
TriFox Dev Team <ghostsec2424@gmail.com>
2025-05-10 14:50:44 -0600

    Create render.sdl.rb

---
+++ geckoSEML/render.sdl.rb
@@ -0,0 +1,7 @@
+s_
+
+[3.5] | {
+       display sys.call(api) fn from.desktop gecko-render ui(compile:HTML:CSS:(temp-GeckoCCM3)) 
+       render_desktop env from:GeckoCCm3 temp pile(web) *as ui.vis desktopSEML CCM3_desktop user(int) 
+       on core.sys_call:active-form  
+}
