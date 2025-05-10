202f91b101238628cc7d425e85a77b75dd22b258
TriFox Dev Team <ghostsec2424@gmail.com>
2025-05-10 14:46:41 -0600

    Create reader.sdl.rb

---
+++ geckoSEML/reader.sdl.rb
@@ -0,0 +1,8 @@
+s_
+
+[3.5] | {
+       $get_(mintNU)gecko-render.pkg {
+          render(pile) pool cache_ui(vis) from.JS on:compile 
+          read shell *as ptm(form) class_stop engine_run for env marked(user)desk 
+       }
+}
